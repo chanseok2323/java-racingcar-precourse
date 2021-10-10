5e3ac3571655495730d132b7b95f118e849e3d48
박찬석 <chanseok2323@gmail.com>
2021-10-11 04:17:05 +0900

    레이싱 게임 클래스

---
+++ src/main/java/racinggame/domain/RacingGame.java
@@ -0,0 +1,69 @@
+package racinggame.domain;
+
+import racinggame.view.OutView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class RacingGame {
+    private List<Car> cars;
+    private static final int MOVE_VALUE = 4;
+    private int attemptCnt;
+
+    public RacingGame() {
+        cars = new ArrayList<>();
+    }
+
+    public void ready(List<String> carList, int attempt) {
+        this.attemptCnt = attempt;
+        for(int i=0; i<carList.size(); i++) {
+            cars.add(new Car(carList.get(i)));
+        }
+    }
+
+    public void start() {
+        for(int i=0; i <= attemptCnt; i++) {
+            move();
+            i = getMaxPosition();
+        }
+    }
+
+    private int getMaxPosition() {
+        int position = 0;
+        for(int i=0; i < cars.size(); i++) {
+            position = Math.max(position, cars.get(i).getPosition());
+        }
+        return position;
+    }
+
+    public void move() {
+         for (Car car : cars) {
+            getDecide(car);
+            OutView.viewCarDistance(car);
+          }
+        System.out.println();
+     }
+
+    private void getDecide(Car car) {
+        if(RandomGenerator.createRandom() >= MOVE_VALUE) car.move();
+    }
+
+    public void getResult() {
+        List<String> winners = getWinners();
+        System.out.printf("최종 우승자는 %s 입니다.", String.join(",", winners));
+    }
+
+    public List<String> getWinners() {
+        List<String> winner = new ArrayList<>();
+        for(Car car : cars) {
+            winner.add(getWinner(car));
+        }
+        winner.removeAll(Collections.singletonList(null));
+        return winner;
+    }
+
+    private String getWinner(Car car) {
+        return car.getPosition() == attemptCnt ? car.getName() : null;
+    }
+}
