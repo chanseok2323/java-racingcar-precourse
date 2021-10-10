ba1d20ba61cdb24ac253d787cad7335e1460193c
박찬석 <chanseok2323@gmail.com>
2021-10-11 04:17:14 +0900

    레이싱 게임 실행 클래스

--- src/main/java/racinggame/Application.java
+++ src/main/java/racinggame/Application.java
@@ -1,7 +1,15 @@
 package racinggame;
 
+import racinggame.domain.RacingGame;
+import racinggame.view.InputView;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO 자동차 경주 게임 구현
+        RacingGame racingGame = InputView.createRacingGame();
+        racingGame.start();
+        racingGame.getResult();
     }
 }
